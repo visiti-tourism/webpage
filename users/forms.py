f0b4bf5eec6dabc40cd8ffbbd807dc0d257c71cf
damoklov <mishanya@protonmail.com>
2020-06-18 21:52:50 +0300

    Updated user registration with new forms (using allauth)

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+    surname = forms.CharField(max_length=100)
     email = forms.EmailField()
+    password = forms.PasswordInput()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['name', 'surname', 'email', 'password']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from datetime import datetime
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    created = models.DateTimeField('Created', default=datetime.now)
