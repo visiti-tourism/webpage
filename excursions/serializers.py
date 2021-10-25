ad47a73f15b8f0f6e137609ef8bf74a3b09db28c
Na11a <kuchuna@ukr.net>
2021-10-25 19:22:00 +0300

    create crud Excursions

--- excursions/serializers.py
+++ excursions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from excursions.models import Excursion
+from excursions.models import Excursion, Country, City
 
 
 class ExcursionSerializer(serializers.ModelSerializer):
@@ -11,3 +11,41 @@ class ExcursionSerializer(serializers.ModelSerializer):
                   'price',
                   'end_time',
                   'image_title')
+
+
+class ExcursionDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Excursion
+        fields = '__all__'
+
+        @property
+        def city(self):
+            return self.city.name
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+
+        def get_country(self,obj):
+            return obj.country.name
+
+class ExcursionRestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Excursion
+        fields = '__all__'
+
+class CountryRestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+class CityRestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
--- excursions/views.py
+++ excursions/views.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 from django.views.generic import (
     ListView,
-    DetailView,
-    CreateView
 )
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from rest_framework.decorators import api_view, permission_classes
-from .models import Excursion
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Excursion, Country, City
 from rest_framework.views import APIView
 from django.http.response import JsonResponse
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ExcursionSerializer
-from rest_framework import status
+from .serializers import (ExcursionSerializer,
+                          ExcursionDetailSerializer,
+                          CountrySerializer,
+                          CitySerializer,
+                          ExcursionRestSerializer,
+                          CountryRestSerializer,
+                          CityRestSerializer)
+
+from rest_framework import viewsets
 from django.http import Http404
 
 
@@ -22,18 +26,23 @@ def superuser_or_agency(user):
 
 class ExcursionView(APIView):
 
-    def get(self, request, format=None):
-        excursions = Excursion.objects.all().order_by('-start_time')
-        excursions_serializer = ExcursionSerializer(excursions, many=True)
-        return JsonResponse({'excursions': excursions_serializer.data})
+    filter_fields = ('name','price')
 
+    def get(self,request,format=None):
+        queryset = Excursion.objects.all()
+        ef = ExcursionsFilter()
+        filtered_queryset = ef.filter_queryset(request, queryset, self)
+        if filtered_queryset.exists():
+            serializer = ExcursionSerializer(filtered_queryset, many=True)
+            return Response({'excursions':serializer.data},status=200)
+        return Response(status=400)
 
     def post(self, request, format=None):
         excursion = request.data
         serializer = ExcursionSerializer(data=excursion)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({'excursion': excursion}, status=200)
+            return JsonResponse({'excursion': excursion}, status=201)
         else:
             return JsonResponse({}, status=400)
 
@@ -52,28 +61,76 @@ def get_object(pk):
         raise Http404
 
 
-
-
-
 class ExcursionDetailView(APIView):
 
-    def get(self, request, pk):
+    def get(self, request, pk, format=None):
         excursion = get_object(pk=pk)
-        serializer = ExcursionSerializer(excursion)
-        return JsonResponse({'excursion': serializer.data})
+        serializer = ExcursionDetailSerializer(excursion)
+        return JsonResponse({'excursion': serializer.data}, status=200)
 
-
-    def put(self, request, pk,format=None):
+    def put(self, request, pk, format=None):
         excursion = get_object(pk=pk)
         data = request.data
-        serializer = ExcursionSerializer(excursion,data=data)
+        serializer = ExcursionDetailSerializer(excursion, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse({'excursion': serializer.data}, status=200)
-        return JsonResponse(serializer.errors,status=400)
+        return JsonResponse(serializer.errors, status=400)
 
-    def delete(self, request, pk,format=None):
+    def delete(self, request, pk, format=None):
         excursion = get_object(pk)
         excursion.delete()
         return Response(status=200)
 
+class ExcursionRestDetail(viewsets.ModelViewSet):
+    queryset = Excursion.objects.all()
+    serializer_class = ExcursionRestSerializer
+
+class CountryView(APIView):
+    def get(self,request):
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+        return JsonResponse({'countries':serializer.data},status=200)
+
+    def post(self,request):
+        country = request.data
+        serializer = CountrySerializer(data = country)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=201)
+        return Response(status=400)
+class CountryRestView(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountryRestSerializer
+
+class CityView(APIView):
+    def get(self,request):
+        cities = City.objects.all()
+        serializer = CitySerializer(cities, many=True)
+        return JsonResponse({'cities':serializer.data}, status=200)
+    def post(self,request):
+        city = request.data
+        serializer = CitySerializer(data = city)
+        if serializer.is_valid():
+            return Response(status=201)
+        return Response(status=400)
+
+class CityRestView(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CityRestSerializer
+
+class CityFilterView(APIView):
+    def get(self, request, city_name):
+        city = City.objects.get(name = city_name)
+        if not city:
+            return Response(status=404)
+        excursions = Excursion.objects.filter(city=city)
+        serializer = ExcursionSerializer(excursions, many=True)
+        return JsonResponse({'excursions':serializer.data},status=200)
+
+class ExcursionsFilter(DjangoFilterBackend):
+    def filter_queryset(self,request,queryset,view):
+        filter_class = self.get_filter_class(view,queryset)
+        if filter_class:
+            return filter_class(request.query_params,queryset=queryset,request=request).qs
+        return queryset
