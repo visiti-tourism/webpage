ce32527adc0a10e86c5e9f2bd5f60fa5d68f84e8
Na11a <kuchuna@ukr.net>
2021-11-04 22:19:25 +0200

    hotfix

--- website/views.py
+++ website/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render_to_response, render, redirect
 
 
-def index(request):
-    return render_to_response('home.html')
-
 
 def app(request):
     return render_to_response('app.html')
