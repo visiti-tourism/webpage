aef4d190610044449a4bcdbbef81a08f4dd5987d
Na11a <kuchuna@ukr.net>
2021-11-29 21:20:40 +0200

    set null to city

--- excursions/models.py
+++ excursions/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=30,)
+    name = models.CharField(max_length=30, )
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Country(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=30, default="None")
-    country = models.ForeignKey(Country, on_delete=models.)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -22,10 +22,10 @@ class Excursion(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, default="None")
     price = models.IntegerField()
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
     start_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now)
-    end_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now)
-    image_title = models.ImageField(upload_to="static/images/excursions/", default="None")
+    end_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=None,null=True)
+    image_title = models.ImageField(upload_to="static/images/excursions/", default="None",null=True)
 
     def __str__(self):
         return self.name
